8ecb2b30e389dd0679c57c874396ec1ecb1e26a7
Mario Jimenez <mario.aljim14@gmail.com>
2023-03-17 09:32:30 -0600

    aplicacion terminada

--- app/src/main/java/com/wolf/MainActivity.java
+++ app/src/main/java/com/wolf/MainActivity.java
@@ -8,25 +8,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        EditText editNombre = findViewById(R.id.user_name);
-        TextView txtSaludo = findViewById(R.id.txt_saludo);
-        Button boton = findViewById(R.id.btn_saludar);
+        EditText temperature = findViewById(R.id.temperature);
+        TextView result = findViewById(R.id.txt_result);
+        Button boton = findViewById(R.id.btn_convert);
 
 
         boton.setOnClickListener(v->{
-            String nombre = editNombre.getText().toString();
-            if(!nombre.isEmpty()) {
-                String saludo = getString(R.string.welcome_message,nombre.toUpperCase());
-                txtSaludo.setText(saludo);
+            double conversion;
+            double conversionResult = 0;
+            String farentheit = temperature.getText().toString();
+            if(!farentheit.isEmpty()) {
+                String success = getString(R.string.success);
+                try {
+                    conversion = Double.parseDouble(farentheit);
+                    conversionResult = ((conversion - 32)* 5) / 9;
+                    Toast.makeText(this, conversionResult + " ",
+                            Toast.LENGTH_SHORT).show();
+                } catch(NumberFormatException nfe) {
+                    conversion = 0;
+                    Toast.makeText(this, R.string.fail,
+                            Toast.LENGTH_SHORT).show();
+                }
+                result.setText(conversionResult + "");
             }else{
-                Toast.makeText(this, R.string.your_should_write_your_name,
+                Toast.makeText(this, R.string.fail,
                         Toast.LENGTH_SHORT).show();
             }
         });
